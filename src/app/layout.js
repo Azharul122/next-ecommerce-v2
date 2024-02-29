fefbc160f265aab1666ece04fe6ef218f31c57ac
Azharul Islam <azharulip@gmail.com>
2024-02-29 23:23:16 +0600

    Metadata issue fixed

--- src/app/layout.js
+++ src/app/layout.js
@@ -15,7 +15,7 @@ import { Inter } from 'next/font/google'
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body>
         <SkeletonTheme >
           <AuthProvider>
             <WishProvider>
